f746f289bcecf2b56eee22d873bc1df5c377862d
Josué <josueavx@gmail.com>
2022-08-07 17:48:42 -0300

    v1

--- services.py
+++ services.py
@@ -5,6 +5,7 @@ from google_auth_oauthlib.flow import Flow
 from flask import Flask, request, jsonify, render_template
 import requests
 import time
+from utils import CONST
 
 class values:
     token = ""
@@ -52,10 +53,43 @@ def chat():
         text = request.args.get('input_text')
         credencial = values.token
 
-        retorno = main.Conversa(credencial, text)
+        #retorno = main.Conversa(credencial, text)
+        payload = payload = {
+            "queryParams": {
+                "source": "DIALOGFLOW_CONSOLE",
+                "timeZone": "America/Fortaleza",
+                "sentimentAnalysisRequestConfig": {
+                    "analyzeQueryTextSentiment": True
+                }
+            },
+            "queryInput": {
+                "text": {"text": text, "languageCode": "pt-br"}
+            }
+        }
+
+        headers = {
+            'accept': 'application/json text/plain, */*',
+            'content-type': 'application/json; charset=UTF-8',
+            'authorization': 'Bearer ' + credencial
+        }
+
+        res = requests.post(CONST.URL, headers=headers, json=payload)
         
-        
-        return {'res': retorno}
+        if(res.status_code == 200):
+            resp = json.loads(res.text)
+            resp_message = resp['queryResult']['fulfillmentMessages']
+    
+            if len(resp_message) > 1:
+                c = 0
+                re = []
+                for i in resp_message:
+                    contagem = resp_message[c]['text']['text'][0]
+                    re.append(contagem)
+                    c += 1
+            else:
+                re = contagem = resp_message[0]['text']['text'][0]
+            
+        return {'res': re}
         
         """
         tip = type(retorno)
